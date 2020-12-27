8001f1139a80aeda0ac635667f9ae0c51b62c901
Kamil <kamilhumbatov@gmail.com>
2020-12-27 18:08:16 +0300

    SLF4j

--- src/main/java/com/example/demo/service/impl/MyEntityServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/MyEntityServiceImpl.java
@@ -6,8 +6,6 @@ import com.example.demo.repository.MyEntityRepository;
 import com.example.demo.service.MyEntityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
